3905c40d0c5173ac77b1fc666d277208724e3237
Adam <adam_tanner@intuit.com>
2022-07-13 19:37:02 +0000

    edited the barchart and enhanced with Phishness

--- app.py
+++ app.py
@@ -6,45 +6,45 @@ from dash import dcc
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
+shows=['Noblesville IN 6-5-22', 'Noblesville IN 6-4-22', 'Noblesville IN 6-3-22', 'Charleston SC 6-1-22']
+phishnet_rating=[3.86, 4.1, 3.791, 3.72]
+songs_in_setlist=[19, 17, 18, 24]
 color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
+color2='yellow'
+mytitle='Phish Show Rating & Songs in Setlist (Prev 4 Shows)'
 
-label1='IBU'
-label2='ABV'
+label1='Rating'
+label2='Setlist'
 
 ########### Set up the chart
 
-def make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle):
-    bitterness = go.Bar(
-        x=beers,
-        y=ibu_values,
+def make_that_cool_barchart(shows, phishnet_rating, songs_in_setlist, color1, color2, mytitle):
+    rating = go.Bar(
+        x=shows,
+        y=phishnet_rating,
         name=label1,
         marker={'color':color1}
     )
-    alcohol = go.Bar(
-        x=beers,
-        y=abv_values,
+    setlist = go.Bar(
+        x=shows,
+        y=songs_in_setlist,
         name=label2,
         marker={'color':color2}
     )
 
-    beer_data = [bitterness, alcohol]
-    beer_layout = go.Layout(
+    phish_data = [rating, setlist]
+    phish_layout = go.Layout(
         barmode='group',
         title = mytitle
     )
 
-    beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-    return beer_fig
+    phish_fig = go.Figure(data=phish_data, layout=phish_layout)
+    return phish_fig
 
 
 ######### Run the function #######
 
 if __name__ == '__main__':
-    fig = make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle)
+    fig = make_that_cool_barchart(shows, phishnet_rating, songs_in_setlist, color1, color2, mytitle)
     fig.write_html('docs/barchart.html')
     print('We successfully updated the barchart!')
